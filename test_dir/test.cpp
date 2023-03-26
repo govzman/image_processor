96c2961d7b58a4901fdb95c05a14d9aa62007a64
Ilya Govzman <govzman@list.ru>
2023-03-26 13:43:43 +0300

    fix path to data images and fix CMake

--- test_dir/test.cpp
+++ test_dir/test.cpp
@@ -12,8 +12,8 @@
 int main() {
     FileWorker file_worker;
 
-    Image image_flag = file_worker.Read("../tasks/image_processor/test_script/data/flag.bmp");
-    Image image_lenna = file_worker.Read("../tasks/image_processor/test_script/data/lenna.bmp");
+    Image image_flag = file_worker.Read("../../data/flag.bmp");
+    Image image_lenna = file_worker.Read("../../data/lenna.bmp");
     {
         FilterNegative filter_negative;
         Image flag_with_filter = image_flag;
@@ -21,8 +21,8 @@ int main() {
 
         filter_negative.Apply(flag_with_filter);
         filter_negative.Apply(lenna_with_filter);
-        assert(flag_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/flag_neg.bmp"));
-        assert(lenna_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/lenna_neg.bmp"));
+        assert(flag_with_filter == file_worker.Read("../../data/flag_neg.bmp"));
+        assert(lenna_with_filter == file_worker.Read("../../data/lenna_neg.bmp"));
     }
     {
         FilterGrayscale filter_grayscale;
@@ -31,8 +31,8 @@ int main() {
 
         filter_grayscale.Apply(flag_with_filter);
         filter_grayscale.Apply(lenna_with_filter);
-        assert(flag_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/flag_gs.bmp"));
-        assert(lenna_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/lenna_gs.bmp"));
+        assert(flag_with_filter == file_worker.Read("../../data/flag_gs.bmp"));
+        assert(lenna_with_filter == file_worker.Read("../../data/lenna_gs.bmp"));
     }
     {
         FilterSharpening filter_sharpening;
@@ -41,8 +41,8 @@ int main() {
 
         filter_sharpening.Apply(flag_with_filter);
         filter_sharpening.Apply(lenna_with_filter);
-        assert(flag_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/flag_sharp.bmp"));
-        assert(lenna_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/lenna_sharp.bmp"));
+        assert(flag_with_filter == file_worker.Read("../../data/flag_sharp.bmp"));
+        assert(lenna_with_filter == file_worker.Read("../../data/lenna_sharp.bmp"));
     }
     {
         FilterCrop filter_crop;
@@ -52,29 +52,29 @@ int main() {
 
         filter_crop.Apply(flag_with_filter, 100, 100);
         filter_crop.Apply(lenna_with_filter, 999, 1999);
-        assert(flag_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/flag_crop.bmp"));
-        assert(lenna_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/lenna_crop.bmp"));
+        assert(flag_with_filter == file_worker.Read("../../data/flag_crop.bmp"));
+        assert(lenna_with_filter == file_worker.Read("../../data/lenna_crop.bmp"));
     }
     {
         FilterEdgeDetection filter_edge_detection;
         Image flag_with_filter = image_flag;
 
         filter_edge_detection.Apply(flag_with_filter, 0.1);
-        assert(flag_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/flag_edge.bmp"));
+        assert(flag_with_filter == file_worker.Read("../../data/flag_edge.bmp"));
     }
     {
         FilterGauss filter_gauss;
         Image lenna_with_filter = image_lenna;
 
         filter_gauss.Apply(lenna_with_filter, 7.5);
-        assert(lenna_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/lenna_blur.bmp"));
+        assert(lenna_with_filter == file_worker.Read("../../data/lenna_blur.bmp"));
     }
     {
         FilterAnaglyph filter_anaglyph;
         Image lenna_with_filter = image_lenna;
 
         filter_anaglyph.Apply(lenna_with_filter, 15);
-        assert(lenna_with_filter == file_worker.Read("../tasks/image_processor/test_script/data/lenna_anag.bmp"));
+        assert(lenna_with_filter == file_worker.Read("../../data/lenna_anag.bmp"));
     }
     return 0;
 }
